e3a55eda0ccf03eb0e2117130b2ebce5b4b68eaf
Tristan F <LeoDog896@hotmail.com>
2023-02-25 00:00:54 -0500

    chore: import from godot-jigsaw

---
+++ puzzle/Puzzle.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+enum HingeState {
+	EXTENDED = 0,
+	CONTRACTED = 1,
+	NONE = 2
+}
+
+export (Texture) var texture
+
+export (int) var rows = 0
+export (int) var cols = 0
+
+var puzzle_piece := preload("res://puzzle/PuzzlePiece.tscn")
+
+func _reverse_hinge(hinge: int) -> int:
+	return HingeState.EXTENDED if hinge == HingeState.CONTRACTED else HingeState.CONTRACTED
+
+func _ready():
+	var pieces: Array = []
+	
+	for n in rows * cols:
+		var piece := puzzle_piece.instance()
+
+		# we need to pass down context info to the puzzle piece
+		# from the parent to keep it isolated
+		piece.rows = rows
+		piece.cols = cols
+		
+		piece.row = n / cols
+		piece.col = n % cols
+		
+		piece.texture = texture
+		
+		var neighbors := {
+			top = null if piece.row == 0 else pieces[n - cols],
+			left = null if piece.col == 0 else pieces[n - 1],
+		}
+		
+		var image_size := Vector2(texture.get_width(), texture.get_height())
+		
+		piece.piece_scale = image_size / Vector2(cols, rows) * scale
+		
+		# we don't use a dictionary here since different values gives better editing in the editor UI
+		piece.top_hinge    = HingeState.NONE if neighbors.top == null else _reverse_hinge(neighbors.top.bottom_hinge)
+		piece.left_hinge   = HingeState.NONE if neighbors.left == null else _reverse_hinge(neighbors.left.right_hinge)
+		piece.right_hinge  = HingeState.NONE if piece.col == cols - 1 else randi() % 2
+		piece.bottom_hinge = HingeState.NONE if piece.row == rows - 1 else randi() % 2
+		
+		piece.position = (piece.piece_scale * 3 * Vector2(piece.col, piece.row)) - (image_size * scale)
+		
+		pieces.append(piece)
+	
+	for piece in pieces:
+		add_child(piece)
---
+++ puzzle/PuzzlePiece.gd
@@ -0,0 +1,67 @@
+extends Polygon2D
+
+enum HingeState {
+	EXTENDED = 0,
+	CONTRACTED = 1,
+	NONE = 2
+}
+
+export (int) var rows
+export (int) var cols
+
+export (int) var row
+export (int) var col
+
+export(HingeState) var top_hinge = HingeState.NONE
+export(HingeState) var left_hinge = HingeState.NONE
+export(HingeState) var right_hinge = HingeState.NONE
+export(HingeState) var bottom_hinge = HingeState.NONE
+
+export var piece_scale: Vector2
+
+func hinge(type: int, direction: Vector2) -> PoolVector2Array:
+	# this is technically a "right hinge", so we can rotate it to be whatever hinge we want
+	
+	var angle := direction.angle()
+	var pool = PoolVector2Array()
+	
+	# because puzzle pieces can be oriented differently we need to swap width and height depending on the direction
+	var current_scale := piece_scale if direction.y == 0 else Vector2(piece_scale.y, piece_scale.x)
+	pool.append(current_scale.rotated(angle))
+	
+	# since our puzzle piece is around (0, 0), we can use current_scale / 4 to define the hinge boundaries
+	if type != HingeState.NONE:
+		pool.append_array([
+			Vector2(current_scale.x, current_scale.y / 4).rotated(angle),
+			Vector2(current_scale.x + current_scale.x / 2 * sign(type - 0.5), current_scale.y / 4).rotated(angle),
+			Vector2(current_scale.x + current_scale.x / 2 * sign(type - 0.5), -current_scale.y / 4).rotated(angle),
+			Vector2(current_scale.x, -current_scale.y / 4).rotated(angle),
+		])
+	
+	return pool
+
+func _ready() -> void:
+	polygon = (
+		hinge(right_hinge, Vector2.RIGHT)
+		+ hinge(top_hinge, Vector2.UP)
+		+ hinge(left_hinge, Vector2.LEFT)
+		+ hinge(bottom_hinge, Vector2.DOWN)
+	)
+	
+	# we keep track of our own UV array since we can't append to it directly (the getter returns a clone)
+	var local_uv := []
+	
+	var image_width: int = texture.get_width() / cols
+	var image_height: int = texture.get_height() / rows
+	
+	for vertex in polygon:
+		var normalized_vertex: Vector2 = (vertex / (piece_scale)) * (Vector2(image_width, image_height) / 2)
+		local_uv.append(
+			normalized_vertex 
+			+ Vector2(
+				image_width / 2.0 + (image_width * col),
+				image_height / 2.0 + (image_height * row)
+			)
+		)
+	
+	uv = local_uv
